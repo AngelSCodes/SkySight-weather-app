382da8256c2d8d25cc2a514f59bb6fdff52c93ba
AngelSCodes <158100915+AngelSCodes@users.noreply.github.com>
2024-05-16 21:42:59 +0200

    Added search function with API

--- src/index.js
+++ src/index.js
@@ -1,8 +1,29 @@
+function updateWeather(response){
+let currentTemperature = document.querySelector("#current-weather-temperature");
+let temperature = response.data.temperature.current;
+let cityValue = document.querySelector("#weather-app-city");
+
+cityValue.innerHTML = response.data.city;
+currentTemperature.innerHTML = Math.round(temperature);
+}
+
+
+function searchCity(city){
+    let apiKey = "ta0fa480a5ddb93fe2fa0b479bf3b70o";
+    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  
+    axios.get(apiUrl).then(updateWeather);
+}
+
+
 function displayCity(event) {
     event.preventDefault();
-    let searchCity = document.querySelector("#search-input");
-    let city = document.querySelector("#weather-app-city");
-   city.innerHTML = searchCity.value;
+    let searchInput = document.querySelector("#search-input");
+   
+
+   searchCity(searchInput.value);
 }
-let searchInput = document.querySelector("#search-form");
-searchInput.addEventListener("submit", displayCity);
+let searchFormInput = document.querySelector("#search-form");
+searchFormInput.addEventListener("submit", displayCity);
+
+searchCity("Pretoria");
