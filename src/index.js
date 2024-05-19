d3a8d10f8b16758b90bfc62af9bd5b53763989fe
AngelSCodes <158100915+AngelSCodes@users.noreply.github.com>
2024-05-19 20:24:52 +0200

    added more "weather-app" functionality

--- src/index.js
+++ src/index.js
@@ -2,10 +2,43 @@ function updateWeather(response){
 let currentTemperature = document.querySelector("#current-weather-temperature");
 let temperature = response.data.temperature.current;
 let cityValue = document.querySelector("#weather-app-city");
+let condition = document.querySelector("#weather-condition");
+let humidity = document.querySelector("#humidity");
+let windSpeed = document.querySelector("#windSpeed");
+let timeElement = document.querySelector("#time");
+let date = new Date(response.data.time *1000);
+
+
 
 cityValue.innerHTML = response.data.city;
 currentTemperature.innerHTML = Math.round(temperature);
+condition.innerHTML = response.data.condition.description;
+humidity.innerHTML = `${response.data.temperature.humidity}%`;
+windSpeed.innerHTML = `${response.data.wind.speed}km/h`;
+timeElement.innerHTML = formatDate(date);
 }
+
+function formatDate(date){
+let minutes = date.getMinutes();
+let hours = date.getHours();
+let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+];
+let day = days[date.getDay()];
+
+if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+return `${day} ${hours}:${minutes}`;
+}
+
 
 
 function searchCity(city){
